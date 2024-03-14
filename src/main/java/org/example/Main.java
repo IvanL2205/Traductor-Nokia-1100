2d6c5b853bb9dedfa9c196f8d64eea95552bcb0a
ivan.lopez <ivan.lopez2205@gmail.com>
2024-03-14 16:28:22 -0500

    Archivo Ejecutable

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A continuación deberá ingresar la cantidad de números de cada letra. " +
+                "\nPara esto deberá presionar ingresar la cantidad de números correspondientes de cada letra " +
+                "\n Por ejemplo para obtener la letra \"a\" debe ingresar un solo dos(2), por el contrario, " +
+                "\nsi desea obtener la letra \"c\" deberá ingresar tres veces dos(222). para continuar " +
+                "\ningresando cada código y formar su frase/palabra, debe separar los códigos por guines medios \"-\"."+
+                "\nSi desea ingresar número naturales, por favor digitelos seguidos del uno(1)");
+
+        System.out.println("Su palabra/frase es: " + Traductor.teclado());
+
+    }
+}
